e078f4f241f2e565d5b86b576a6b486d4c107bee
MangelC25 <150280388+MangelC25@users.noreply.github.com>
2024-01-19 00:44:14 -0500

    commit 20
    Funtion Theme Page 2.0 (header -navbar, Home and Abour)

--- JS/Theme.js
+++ JS/Theme.js
@@ -6,47 +6,62 @@ const bgsecundarydark = getComputedStyle(document.documentElement).getPropertyVa
 const textcolor = getComputedStyle(document.documentElement).getPropertyValue('--text-color');
 const maincolor = getComputedStyle(document.documentElement).getPropertyValue('--main-color');
 
+const bgdegradado = getComputedStyle(document.documentElement).getPropertyValue('--bg-degradado');
+
 const bgnavligth = getComputedStyle(document.documentElement).getPropertyValue('--bg-navbar-light');
 const bgprimaryligth = getComputedStyle(document.documentElement).getPropertyValue('--bg-primary-light');
 const bgsecundaryligth = getComputedStyle(document.documentElement).getPropertyValue('--bg-secundary-light');
 const header = document.querySelector(".header");
+const Home = document.querySelector(".Home");
+const About = document.querySelector(".About");
 const navitems = document.querySelectorAll(".header .navbar #navbar .navbar-nav .nav-item");
 
 let navLinks = document.querySelectorAll('.nav-link');
 
+
+
 Btheme.addEventListener("click",() =>{
     changetheme();
+    
 })
 
 function changetheme() {
+    changethemeheader();
+    changethemeHome();
+    changethemeAbout();
+};
+
+function changethemeheader(){
     let styles = window.getComputedStyle(header);
     const currentBgColor = rgbToHex(styles.backgroundColor);
+    let Bthemeheader = header.getElementsByClassName("Btheme")[0];
 
     if (currentBgColor === bgnavdark) {
         header.style.backgroundColor = bgnavligth;
+        Bthemeheader.querySelector("i").className = "bx bxs-sun";
         navitems.forEach(item => {
             item.addEventListener("mouseover", () => {
                 item.style.backgroundColor = "#ffffff";
                 item.style["border-radius"] = "5rem";
             });
             item.addEventListener("mouseout", () => {
-                item.style.backgroundColor = ""; // Restaura el color de fondo original al salir del hover
+                item.style.backgroundColor = ""; 
             });
         });
 
         navLinks.forEach(link => {
             link.addEventListener("mouseover", () => {
                 link.style.color = bgnavdark;
-                link.style.textShadow = "none"; // O el nuevo valor que desees
+                link.style.textShadow = "none"; 
             });
             link.addEventListener("mouseout", () => {
                 link.style.color = textcolor;
-                link.style.textShadow = "none"; // O el nuevo valor que desees
+                link.style.textShadow = "none"; 
             });
         });
 
-
     } else if (currentBgColor === bgnavligth) {
+        Bthemeheader.querySelector("i").className = "bx bxs-moon";
         header.style.backgroundColor = bgnavdark;
         navitems.forEach(item => {
             item.addEventListener("mouseover", () => {
@@ -54,19 +69,124 @@ function changetheme() {
                 item.style["border-radius"] = "5rem";
             });
             item.addEventListener("mouseout", () => {
-                item.style.backgroundColor = ""; // Restaura el color de fondo original al salir del hover
+                item.style.backgroundColor = ""; 
             });
         });
 
         navLinks.forEach(link => {
             link.addEventListener("mouseover", () => {
                 link.style.color = maincolor;
-                link.style.textShadow = "rgba(0, 183, 255, 0.533) 0px 0px 5.21269px, rgba(0, 183, 255, 0.533) 0px 0px 10.4254px, rgba(0, 183, 255, 0.533) 0px 0px 15.6381px"; // O el nuevo valor que desees
+                link.style.textShadow = '0 0 1rem ' + maincolor + ', 0 0 2rem ' + maincolor + ', 0 0 3rem ' + maincolor; 
             });
             link.addEventListener("mouseout", () => {
                 link.style.color = textcolor;
-                link.style.textShadow = "none"; // O el nuevo valor que desees
+                link.style.textShadow = "none"; 
             });
+        });
+    }
+}
+
+function changethemeHome(){ 
+    
+    const rds = document.querySelector('.home-rds');
+
+    let styles = window.getComputedStyle(Home);
+    let style2 = window.getComputedStyle(rds);
+
+    const currentBackground = styles.background; 
+    const currentBackground2 = style2.background;
+    const currentBgPrimary1 = hexToRgb(bgprimarydark);
+    const currentBgPrimary2 = hexToRgb(bgprimaryligth);
+
+    const bgdegradadolight = `linear-gradient(to top, rgb(255, 255, 255), ${currentBgPrimary2})`;
+
+    const textElements = Home.querySelectorAll('h1, h2, p, a');
+    const btn = Home.querySelectorAll('a');
+
+    const linksbtm = rds.querySelectorAll('a');
+    
+    if (currentBackground.includes(currentBgPrimary1)) {
+        Home.style.background = bgdegradadolight;
+        textElements.forEach(element => {
+            element.style.color = bgprimarydark; 
+        });
+        btn.forEach(item => {
+            item.style.color = bgprimarydark; 
+            item.addEventListener("mouseover", () => {
+                item.style.color = textcolor;
+            });
+            item.addEventListener("mouseout", () => {
+                item.style.color = bgprimarydark
+            });
+        });
+
+        linksbtm.forEach(item => {
+            item.style.color = bgprimarydark; 
+            item.addEventListener("mouseover", () => {
+                item.style.backgroundColor = bgsecundarydark;
+                item.style.boxShadow = `0 0 5rem `+ maincolor;
+            });
+            item.addEventListener("mouseout", () => {
+                item.style.backgroundColor = "transparent"; 
+                item.style.boxShadow = "none";
+            });
+        });
+    } else if (currentBackground.includes(currentBgPrimary2)){
+        Home.style.background = bgdegradado;
+        textElements.forEach(element => {
+            element.style.color = textcolor; 
+        });
+        btn.forEach(item => {
+            item.style.color = maincolor; 
+            item.addEventListener("mouseover", () => {
+                item.style.color = textcolor;
+            });
+            item.addEventListener("mouseout", () => {
+                item.style.color = maincolor;
+            });
+        });
+
+        linksbtm.forEach(item => {
+            item.style.color = maincolor; 
+            item.addEventListener("mouseover", () => {
+                item.style.backgroundColor = bgsecundarydark;
+                item.style.boxShadow = `0 0 5rem `+ maincolor
+            });
+            item.addEventListener("mouseout", () => {
+                item.style.backgroundColor = "transparent"; 
+                item.style.boxShadow = "none";
+            });
+        });
+    }
+
+    console.log(currentBackground2);
+
+}
+
+function changethemeAbout(){
+    let styles = window.getComputedStyle(About);
+    const currentBackground = styles.background;
+
+
+    const currentBgSecundary1 = hexToRgb(bgsecundarydark);
+    const currentBgSecundary2 = hexToRgb(bgsecundaryligth);
+
+    const bgdegradadodark = `linear-gradient(to top, ${currentBgSecundary1}, rgb(12, 30, 46))`;
+    const bgdegradadolight = `linear-gradient(to top, ${currentBgSecundary2}, rgb(235, 246, 255))`;
+    
+
+    const textElements = About.querySelectorAll('h1, p, #A, .carousel-control-prev-icon, .carousel-control-next-icon');
+
+    if (currentBackground.includes(currentBgSecundary1)) {
+        About.style.background = bgdegradadolight;
+        textElements.forEach(element => {
+            element.style.color = bgprimarydark; 
+        });
+
+    } else if (currentBackground.includes(currentBgSecundary2)){
+        About.style.background = bgdegradadodark;
+        textElements.forEach(element => {
+            element.style.color = textcolor; 
         });
     }
 }
@@ -74,4 +194,14 @@ function changetheme() {
 function rgbToHex(rgb) {
     const rgbArray = rgb.match(/\d+/g);
     return "#" + rgbArray.map(num => parseInt(num).toString(16).padStart(2, '0')).join('');
+}
+
+function hexToRgb(hex) {
+    hex = hex.replace(/^#/, '');
+    let pairs = hex.match(/.{1,2}/g);
+    let r = parseInt(pairs[0], 16);
+    let g = parseInt(pairs[1], 16);
+    let b = parseInt(pairs[2], 16);
+
+    return `rgb(${r}, ${g}, ${b})`;
 }
